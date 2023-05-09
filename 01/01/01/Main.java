3304a1f7c9670cf146071a61ba19a2012c9000de
HimanaHito74 <132960288+HimanaHito74@users.noreply.github.com>
2023-05-09 09:56:30 +0900

    01

--- 01/01/01/Main.java
+++ 01/01/01/Main.java
@@ -1,4 +1,27 @@
+import java.util.List;
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main{
+    private static final int MAX_RETRIES = 3;
+
     public static void main(String... args){
+        final int amount_of_received = 10000;
+        final List<int> currency = new ArrayList<int>(Arrays.asList(10000, 5000, 2000, 1000, 500, 100, 50, 10, 5, 1));
+        final List<int> excluded_currency = new ArrayList<int>(Arrays.asList(2000));
+
+        Scanner input = new Scanner(System.in);
+        String input_string;
+        final int amount_of_payment;
+
+        for(int i=0; i<MAX_RETRIES; i++){
+            try{
+                input_string = input.nextLine();
+                amount_of_payment = Integer.parseInt(input_string);
+            }catch(NumberFormatException exception){
+                System.out.println("");
+            }
+        }
     }
 }
