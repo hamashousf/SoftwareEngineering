5794cb0e0894698ec5efab5cdc043e10d97bafdb
HimanaHito74 <132960288+HimanaHito74@users.noreply.github.com>
2023-05-09 10:21:03 +0900

    commit

--- 01/01/01/Main.java
+++ 01/01/01/Main.java
@@ -8,20 +8,22 @@ public class Main{
 
     public static void main(String... args){
         final int amount_of_received = 10000;
-        final List<int> currency = new ArrayList<int>(Arrays.asList(10000, 5000, 2000, 1000, 500, 100, 50, 10, 5, 1));
-        final List<int> excluded_currency = new ArrayList<int>(Arrays.asList(2000));
+        final List<Integer> currency = new ArrayList<Integer>(Arrays.asList(10000, 5000, 2000, 1000, 500, 100, 50, 10, 5, 1));
+        final List<Integer> excluded_currency = new ArrayList<Integer>(Arrays.asList(2000));
 
         Scanner input = new Scanner(System.in);
         String input_string;
-        final int amount_of_payment;
 
         for(int i=0; i<MAX_RETRIES; i++){
             try{
                 input_string = input.nextLine();
-                amount_of_payment = Integer.parseInt(input_string);
+                int amount_of_payment = Integer.parseInt(input_string);
+                if(amount_of_payment > amount_of_received){
+                    System.out.printf("Please enter a value less than %d\n", ); //TODO: THIS CODE IS NOT COMPLETED!
+                }
+                break;
             }catch(NumberFormatException exception){
-                System.out.printf("Invalid value. Remaining Attempts: %d", MAX_RETRIES-i);
-            }
+                System.out.printf("Invalid value. Remaining Attempts: %d\n", MAX_RETRIES-i);
         }
     }
 }
